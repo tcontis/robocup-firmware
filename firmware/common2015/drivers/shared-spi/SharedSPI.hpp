957ec39982dcc9938ff64388e9c3c3e519e577af
jgkamat <jaygkamat@gmail.com>
2017-04-09 15:32:16 -0400

    Try increasing the priority of the Shared SPI thread

--- firmware/common2015/drivers/shared-spi/SharedSPI.hpp
+++ firmware/common2015/drivers/shared-spi/SharedSPI.hpp
@@ -39,14 +39,25 @@ public:
     }
 
     void chipSelect() {
+
+        const osThreadId id = Thread::gettid();
+        const auto priority = osThreadGetPriority(id);
+        //const osPriority threadPriority = _txThread.get_priority();
+        //osThreadSetPriority
+        osStatus tState = osThreadSetPriority(id, osPriorityRealtime);
         _spi->lock();
+        _priority = priority;
+        _id = id;
         _spi->frequency(_frequency);
         _cs = _csAssertValue;
     }
 
     void chipDeselect() {
+        const osThreadId id = _id;
+        const auto priority = _priority;
         _cs = !_csAssertValue;
         _spi->unlock();
+        osStatus tState = osThreadSetPriority(id, priority);
     }
 
     /// Set the SPI frequency for this device
@@ -58,6 +69,8 @@ protected:
 
 private:
     int _csAssertValue;
+    osPriority _priority;
+    osThreadId _id;
 
     /// The SPI bus frequency used by this device.
     /// This default value is the same as the mbed's default (1MHz).
