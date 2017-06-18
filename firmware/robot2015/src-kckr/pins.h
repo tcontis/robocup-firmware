b10e6e4524dee9cb1685058b5a2e1e35164529c0
Jeremy Feltracco <jpfeltracco@gatech.edu>
2017-06-18 16:05:56 -0400

    Reimplement SPI and add more pins.

--- firmware/robot2015/src-kckr/pins.h
+++ firmware/robot2015/src-kckr/pins.h
@@ -3,9 +3,9 @@
 #include <avr/io.h>
 
 /* Inputs */
-//#define N_KICK_CS_PIN (PORTA7)  // pin 15
-//#define KICK_MOSI_PIN (PORTA6)  // pin 7, not actually used in code
-//#define V_MONITOR_PIN (PORTB1)  // pin 11
+#define N_KICK_CS_PIN (PA6)
+//#define KICK_MOSI_PIN (PORTA6)
+#define V_MONITOR_PIN (PA0)
 // active low buttons
 #define DB_KICK_PIN (PA3)  // should send a normal kick commmand
 // #define DB_CHIP_PIN (PORT)  // should send a normal chip command
@@ -15,14 +15,14 @@
 
 /* Outputs */
 #define CHARGE_PIN (PB2)
-// #define CHIP_PIN (PORT)    // pin 12
-#define KICK_PIN (PB1)    // pin 13
+// #define CHIP_PIN (PORT)
+#define KICK_PIN (PB1)
 
 /* Tri-State */
-//#define KICK_MISO_PIN (PORTA5)  // pin 8
+#define KICK_MISO_PIN (PA2)  // pin 8
 
 /* Interrupts for PCMASK0 or PCMASK1 */
 // #define INT_N_KICK_CS (PCINT7)
-// #define INT_DB_KICK (PCINT8)
+#define INT_DB_KICK (PCINT3)
 // #define INT_DB_CHIP (PCINT9)
-// #define INT_DB_CHG (PCINT10)
+#define INT_DB_CHG (PCINT1)
