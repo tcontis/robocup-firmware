3ea26c6135f166195793fe98318e1115bff281b8
Daniel Yang <danielyang99@outlook.com>
2020-01-14 18:24:29 -0500

    Changed atmega pins

--- kicker/pins.h
+++ kicker/pins.h
@@ -9,33 +9,33 @@
 
 /* hv monitoring */
 #define V_MONITOR_PIN (PA6)
-#define HV_IND_MAX (PA5)
-#define HV_IND_HIGH (PA4)
+#define HV_IND_MAX (PA1)
+#define HV_IND_HIGH (PA2)
 #define HV_IND_MID (PA3)
-#define HV_IND_LOW (PA2)
-#define HV_IND_MIN (PA1)
+#define HV_IND_LOW (PA4)
+#define HV_IND_MIN (PA5)
 
 /* hv reg */
-#define LT_CHARGE (PD0)
-#define LT_DONE_N (PD1)
-#define LT_FAULT_N (PD2)
+#define LT_CHARGE (PD5)
+#define LT_DONE_N (PD6)
+#define LT_FAULT_N (PD7)
 
 /* hv out */
-#define KICK_PIN (PC4)
-#define CHIP_PIN (PC5)
+#define KICK_PIN (PB0)
+#define CHIP_PIN (PB1)
 
 /* ball sense */
 #define BALL_SENSE_TX (PD4)
 #define BALL_SENSE_RX (PA0)
-#define BALL_SENSE_LED (PD7)
+#define BALL_SENSE_LED (PB3)
 
 /* debug */
 #define DB_SWITCH (PC0)
-#define DB_CHG_PIN (PC1)
-#define DB_KICK_PIN (PC2)
-#define DB_CHIP_PIN (PC3)
+#define DB_CHG_PIN (PD3)
+#define DB_KICK_PIN (PD2)
+#define DB_CHIP_PIN (PB2)
 
-#define MCU_GREEN (PD3)
-#define MCU_YELLOW (PD5)
-#define MCU_RED (PD6)
+#define MCU_GREEN (PA7)
+#define MCU_YELLOW (PC7)
+#define MCU_RED (PC6)
 
